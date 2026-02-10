a2ed67e46ccd292a53e5bf300a35fbf54153a42c
malleswari <maddikirimalleswari4@gmail.com>
2026-02-10 13:31:14 +0530

    jenkins

--- vars/nodeJSEKSPipeline.groovy
+++ vars/nodeJSEKSPipeline.groovy
@@ -11,8 +11,8 @@ def call (Map configMap){
         COURSE = "Jenkins"
         appVersion = ""
         ACC_ID = "372836560231"
-        PROJECT = "roboshop"
-        COMPONENT = "catalogue"
+        PROJECT = configMap.get("project")
+        COMPONENT = configMap.get("component")
     }
     options {
         timeout(time: 10, unit: 'MINUTES')
