6e4a4cef6a14fc6bac8ddaa1a269610104e4f9ee
malleswari <maddikirimalleswari4@gmail.com>
2026-02-11 20:03:35 +0530

    jenkins

--- vars/EKSDeploy.groovy
+++ vars/EKSDeploy.groovy
@@ -45,6 +45,19 @@ def call (Map configMap){
                 }
             }
 
+            stage('Functional Testing'){
+                when{
+                    expression { deploy_to == "dev" }
+                }
+                steps{
+                    script{
+                        sh """
+                            echo "functional tests in DEV environment"
+                        """
+                    }
+                }
+            }
+
         }
 
         post {
